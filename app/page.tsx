592d6de52af154c2a2ae289aece3e4789b7fb72f
azzahradins <azzahradindas@gmail.com>
2023-04-10 10:26:27 +0700

    [init]

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      sorry but i got nothing here... i'm currently working on something else...
+    </main>
+  )
+}
